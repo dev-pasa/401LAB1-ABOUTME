b745be65b25b2862eae2317cf330ee7188f403b6
dev-pasa <dev5@uw.edu>
2019-01-08 08:43:25 -0800

    some changes

--- AboutMe/Program.cs
+++ AboutMe/Program.cs
@@ -6,16 +6,20 @@ namespace AboutMe
     {
         static void Main(string[] args)
         {
+            //Ask();
             Ask();
         }
+
+       
 
         static void Ask()
         {
             int count = 0;
             Console.WriteLine("Hi! My name is Dev and I am a learning software developer.");
             Console.WriteLine("I will ask you some questions to tell you about me.");
+
             try
-            {
+            {   //Question 1       
                 Console.WriteLine("How old am I? Enter a number: ");
                 string ageString = Console.ReadLine();
                 int ageNum = Int32.Parse(ageString);
@@ -29,11 +33,13 @@ namespace AboutMe
                     count++;
                 }
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 Console.WriteLine("Wrong Format!!");
             }
-            
+
+            //Question 2  
+            Console.WriteLine(" ");
             Console.WriteLine("Where do I live?");
             string location = Console.ReadLine();
             if(location == "Puyallup" || location == "Tacoma" || location == "tacoma" || location == "puyallup")
@@ -46,9 +52,100 @@ namespace AboutMe
                 Console.WriteLine("Wrong guess!");
             }
 
+            //Question 3
+            Console.WriteLine(" ");
             Console.WriteLine("Did I server in the Army?");
-            string veteran = Console.ReadLine();
+            Console.WriteLine("Option 1 - I did");
+            Console.WriteLine("Option 2 - No I did not");
+            Console.Write("Enter your selection. Either 1 or 2: ");
+            string userInt = Console.ReadLine();
+            int userSelect = Int32.Parse(userInt);
+
+            try
+            {
+                
+                if(userSelect == 1)
+                {
+                    Console.WriteLine("Your are right");
+                    count++;
+                }
+                else if(userSelect == 2)
+                {
+                    Console.WriteLine("Your are wrong");
+                }
+                else
+                    {
+                    Console.WriteLine("Wrong input");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid option [1,2].");
+            }
+
+            //Question 4  
+            Console.WriteLine(" ");
+            Console.WriteLine("What sports do I watch the most?");
+            Console.WriteLine("Option 1 - Soccer");
+            Console.WriteLine("Option 2 - Football");
+            Console.WriteLine("Option 3 - Basketball");
+            Console.Write("Enter your selection: ");
+            string userIputGame = Console.ReadLine();
+            int userGameInt = Int32.Parse(userIputGame);
+
+            try
+            {
+
+                if (userGameInt == 1)
+                {
+                    Console.WriteLine("Your are right");
+                    count++;
+                }         
+                else
+                {
+                    Console.WriteLine("Wrong answer");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid option [1,2].");
+            }
+
+
+            try
+            {   //Question 5  
+                Console.WriteLine(" ");
+                Console.WriteLine("What do I enjoy most about programming?");
+                Console.WriteLine("Option 1 - Team work"); 
+                Console.WriteLine("Option 2 - Problem Solving");
+                Console.WriteLine("Option 3 - Career Porgression");
+                Console.Write("Enter your selection: ");
+                string codingSelect = Console.ReadLine();
+                int codingInt = Int32.Parse(codingSelect);
+                if (codingInt == 2)
+                {
+                    count++;
+                    Console.WriteLine("Correct Answer");
+                }
+                else if (codingInt == 1 || codingInt == 3)
+                {
+                    Console.WriteLine("Wrong answer.");   
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong Format!!");
+            }
+
+
             Console.WriteLine($"Out of 5 questions, you got {count} right.");
+
+
+
         }
 
 
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 401LAB1-ABOUTME
+
+#The name of the app is about me program. 
+
+#The purpose of the program is to give the user some information about me.
+
+#User will have to clone it and run the codes in Visual Studio.
+
+
+#The user will be asked a total of 5 questions. If the user answers the questions correctly, the number of the correct input will be printed out
+# at the end of the program.
+
+#The program does not use any APIs.
