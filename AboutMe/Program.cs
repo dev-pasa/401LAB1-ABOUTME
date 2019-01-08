441882cf4252ef3a2161cf5d666825565800d211
dev-pasa <dev5@uw.edu>
2019-01-08 09:46:27 -0800

    third question asked

--- AboutMe/Program.cs
+++ AboutMe/Program.cs
@@ -12,82 +12,33 @@ namespace AboutMe
         }
 
        
-        static int AgeAsk(int count)
-        {
-            try
-            {   //Question 1       
-                Console.WriteLine("How old am I? Enter a number: ");
-                string ageString = Console.ReadLine();
-                int ageNum = Int32.Parse(ageString);
-                if (ageNum > 30 || ageNum < 30)
-                {
-                    Console.WriteLine("Wrong guess!");
-                }
-                else if (ageNum == 30)
-                {
-                    Console.WriteLine("You are right");
-                    count++;
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Wrong Format!!");
-            }
-            return count ++;
-        }
+        
 
         static void Ask()
         {
-            int count = 0;
+            int count=0;
             Console.WriteLine("Hi! My name is Dev and I am a learning software developer.");
             Console.WriteLine("I will ask you some questions to tell you about me.");
 
-            AgeAsk(count);
-
-            //Question 2  
-            Console.WriteLine(" ");
-            Console.WriteLine("Where do I live?");
-            string location = Console.ReadLine();
-            if(location == "Puyallup" || location == "Tacoma" || location == "tacoma" || location == "puyallup")
+            bool ageAsk = AgeAsk();
+            if(ageAsk == true)
             {
-                Console.WriteLine("You are right");
                 count++;
             }
-            else if(location != "Puyallup" || location != "Tacoma" || location != "tacoma" || location != "puyallup")
+     
+            string location = LocQuestion();
+
+            if(location == "Puyallup" || location == "Tacoma" || location == "tacoma" || location == "puyallup")
             {
-                Console.WriteLine("Wrong guess!");
+                count++;
             }
 
-            //Question 3
-            Console.WriteLine(" ");
-            Console.WriteLine("Did I server in the Army?");
-            Console.WriteLine("Option 1 - I did");
-            Console.WriteLine("Option 2 - No I did not");
-            Console.Write("Enter your selection. Either 1 or 2: ");
-            string userInt = Console.ReadLine();
-            int userSelect = Int32.Parse(userInt);
+            int armyServe = ArmyServer();
+            if(armyServe == 1)
+            {
+                count++;
+            }
 
-            try
-            {
-                
-                if(userSelect == 1)
-                {
-                    Console.WriteLine("Your are right");
-                    count++;
-                }
-                else if(userSelect == 2)
-                {
-                    Console.WriteLine("Your are wrong");
-                }
-                else
-                    {
-                    Console.WriteLine("Wrong input");
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Please enter a valid option [1,2].");
-            }
 
             //Question 4  
             Console.WriteLine(" ");
@@ -154,7 +105,85 @@ namespace AboutMe
 
         }
 
+        static bool AgeAsk()
+        {
+            try
+            {   //Question 1       
+                Console.WriteLine("How old am I? Enter a number: ");
+                string ageString = Console.ReadLine();
+                int ageNum = Int32.Parse(ageString);
+                if (ageNum > 30 || ageNum < 30)
+                {
+                    Console.WriteLine("Wrong guess!");
+                }
+                else if (ageNum == 30)
+                {
+                    Console.WriteLine("You are right");
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Wrong Format!!");
+            }
+            return false;
+        }
 
+        static string LocQuestion()
+        {
+            //Question 2  
+            Console.WriteLine(" ");
+            Console.WriteLine("Where do I live?");
+            string location = Console.ReadLine();
+            if (location == "Puyallup" || location == "Tacoma" || location == "tacoma" || location == "puyallup")
+            {
+                Console.WriteLine("You are right");
+               
+            }
+            else if (location != "Puyallup" || location != "Tacoma" || location != "tacoma" || location != "puyallup")
+            {
+                Console.WriteLine("Wrong guess!");
+            }
+            return location;
+        }
+
+        static int ArmyServer()
+        {
+
+            //Question 3
+            Console.WriteLine(" ");
+            Console.WriteLine("Did I server in the Army?");
+            Console.WriteLine("Option 1 - I did");
+            Console.WriteLine("Option 2 - No I did not");
+            Console.Write("Enter your selection. Either 1 or 2: ");
+            string userInt = Console.ReadLine();
+            int userSelect = Int32.Parse(userInt);
+            int answer = 0;
+
+            try
+            {
+
+                if (userSelect == 1)
+                {
+                    Console.WriteLine("Your are right");
+                   answer = 1;
+                }
+                else if (userSelect == 2)
+                {
+                    Console.WriteLine("Your are wrong");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input");
+                    
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a valid option [1,2].");
+            }
+            return answer;
+        }
     }
     }
 
